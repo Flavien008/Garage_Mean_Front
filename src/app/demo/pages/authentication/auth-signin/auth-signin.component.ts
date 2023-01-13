10bd3efa25226cad84fbf6583cfa77854219f2db
Flavien <flavienrakotoarison8@gmail.com>
2023-01-13 09:34:34 +0300

    Revert "login form controlle"
    This reverts commit 8ac716392ff458401beabe2c19eed420fef667aa.

--- src/app/demo/pages/authentication/auth-signin/auth-signin.component.ts
+++ src/app/demo/pages/authentication/auth-signin/auth-signin.component.ts
@@ -1,34 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-auth-signin',
-    standalone: true,
-    imports: [CommonModule, RouterModule],
-    templateUrl: './auth-signin.component.html',
-    styleUrls: ['./auth-signin.component.scss'],
+  selector: 'app-auth-signin',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './auth-signin.component.html',
+  styleUrls: ['./auth-signin.component.scss'],
 })
-export default class AuthSigninComponent implements OnInit{
-    loginError : boolean = false
-    mdpError : boolean = false
-
-    ngOnInit(): void {
-      throw new Error('Method not implemented.');
-    }
-
-    // display error
-    checkInput(login: string, password : string)
-    {
-        if(login.length == 0) this.loginError = true ; else this.loginError =false
-        if(password.length == 0) this.mdpError = true ; else this.mdpError =false
-        this.login();
-    }
-
-    login(){
-        if(this.loginError == false && this.mdpError == false){
-            console.log(`je vais appeller l'api`);
-            
-        }
-    }
-}
+export default class AuthSigninComponent {}
