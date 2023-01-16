990067274ddfb91941ae767229e16f3b95ebabca
Tendry Arivony <80846844+TendryArivony@users.noreply.github.com>
2023-01-16 14:49:10 +0300

    Upload image base 64

--- src/app/demo/pages/voiture/ajout/ajout.component.ts
+++ src/app/demo/pages/voiture/ajout/ajout.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/theme/shared/shared.module';
 import { NgxDropzoneModule } from 'ngx-dropzone';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-ajout',
   standalone: true,
@@ -12,9 +14,18 @@ import { NgxDropzoneModule } from 'ngx-dropzone';
 
 })
 export default class AjoutComponent {
+  constructor(private http: HttpClient) {}
   files: File[] = [];
 
-  base64textString: String="";
+  base64textString: string="";
+
+  formData = {
+    id_utilisateur: '',
+    type: '',
+    modele: '',
+    matriculation: '',
+    img: ''
+  };
 
   ngOnInit() {
     console.log('Initialisation du composant');
@@ -22,12 +33,12 @@ export default class AjoutComponent {
 
 	onSelect(event) {
 		console.log(event);
-    console.log("atoooooo");
 		this.files.push(...event.addedFiles);
 	}
 
   onFileChange(event) {
-    const files = event.target.files;
+    const files = event.addedFiles;
+    console.log("miov a eto");
     if (files.length > 0) {
         const file = files[0];
         const reader = new FileReader();
@@ -38,12 +49,30 @@ export default class AjoutComponent {
   _handleReaderLoaded(e) {
     const reader = e.target;
     this.base64textString = reader.result;
+    this.formData.img= this.base64textString;
     console.log(this.base64textString);
   }
 
 	onRemove(event) {
 		console.log(event);
-    console.log("atoooooo rayyy");
 		this.files.splice(this.files.indexOf(event), 1);
 	}
+
+  onSubmit() {
+    var token : string;
+    var iduser : string;
+    if(localStorage.getItem('user')!=null){
+        token = JSON.parse(localStorage.getItem('user')).token;
+        iduser = JSON.parse(localStorage.getItem('user')).userId;
+        this.formData.id_utilisateur=iduser;
+    }
+    console.log(this.formData)
+    let headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + token
+    });
+    this.http.post(`${environment.baseUrl}/voitures`, this.formData, { headers: headers }) 
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
 }
